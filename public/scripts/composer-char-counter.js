f3f2b5b59d1640d30ece62200803a4eb9a9402c4
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-26 14:26:32 -0400

    add a counter that console logs number of keypresses

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,14 @@
-console.log("composer-char-counter is linked");
+// console.log("composer-char-counter is linked");
 
 $(document).ready(function() {
   console.log("ready is ready")
+
+  let counter = 140
+  $('#tweet-text').on('keypress', function() {
+
+    // console.log(this)
+    counter--
+    console.log(counter)
+  })
 })
+
