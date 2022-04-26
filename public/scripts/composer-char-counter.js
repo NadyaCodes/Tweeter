3cf525ae129953b68bb88da50c49c64e4392d351
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-26 17:33:43 -0400

    make the counter red when over the max character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,21 @@
-// console.log("composer-char-counter is linked");
-
 let counter = 140
 $(document).ready(function() {
   console.log("ready is ready")
 
 
   $('#tweet-text').on('keypress', function() {
-    // console.log(this)
 
-    // console.log(this)
     counter--
     console.log(counter)
 
-    // $('.counter').on('keypress', function() {
-    //   console.log("Hey");
-    //   $('span').text(counter);
-    // })
-
-    $('output.counter').text(counter)
+    if (counter < 0) {
+      $('output.negative-counter').text(counter)
+      
+    } else {
+      $('output.counter').text(counter)
+    }
     
   })
-
-
-
-
-
 
 })
 
