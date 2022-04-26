a0f3b6cb05bb86dc83d854c09b23e558ada88a6c
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-26 16:48:37 -0400

    make the character counter count down based on user input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,31 @@
 // console.log("composer-char-counter is linked");
 
+let counter = 140
 $(document).ready(function() {
   console.log("ready is ready")
 
-  let counter = 140
+
   $('#tweet-text').on('keypress', function() {
+    // console.log(this)
 
     // console.log(this)
     counter--
     console.log(counter)
+
+    // $('.counter').on('keypress', function() {
+    //   console.log("Hey");
+    //   $('span').text(counter);
+    // })
+
+    $('output.counter').text(counter)
+    
   })
+
+
+
+
+
+
 })
+
 
