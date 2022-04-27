d9df2cbe3490baeead4c1fd381c591947db210ab
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-27 17:31:47 -0400

    add ajax get request to load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const { json } = require("express/lib/response");
 
 $(document).ready(function() {
+  
   console.log("client is ready");
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
 
   const createTweetElement = function(tweetObj) {
     const person = {
@@ -74,7 +50,7 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  // renderTweets(data);
   
 
   const submitTweet = function(event) {
@@ -87,13 +63,20 @@ $(document).ready(function() {
       data: formData,
       url: "/tweets",
     })
-
-      // .then((res) => createTweetElement(res))
-      // .catch((error) = console.log(error))
-
+    
       console.log(formData)
     
   }
-
   $(".form-inline").submit(submitTweet);
+
+  const loadTweets = function () {
+    $.ajax('./tweets', { method: 'GET' })
+    .then(function (data) {
+      renderTweets(data)
+    });
+  }
+
+  loadTweets()
+
+
 });
