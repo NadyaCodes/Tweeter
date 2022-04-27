d394bf9a8db339ea63b2695eb31cde72dd0b41a0
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-27 09:01:12 -0400

    add a function that allows multiple tweets to be rendered in the feed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,73 +6,75 @@
 
 
 $(document).ready(function() {
-  console.log("client is ready")
+  console.log("client is ready");
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
   const createTweetElement = function(tweetObj) {
 
+    const person = {
+      name: tweetObj.user.name,
+      username: tweetObj.user.handle,
+      "single-tweet": tweetObj.content.text,
+      "date-posted": tweetObj.created_at,
+      "small-avatar": tweetObj.user.avatars
+    };
 
-  // const $tweet = $(`<article class="tweets">${tweetObj.content.text}</article>`);
+    const markup = `
+    <article>
+      <header>
+        <div class="tweet-head-left">
+          <div class="small-avatar"><img src="${person["small-avatar"]}"></div>
+          <div class="name">${person.name}</div>
+        </div>
+        <div class="username">${person.username}</div>
+      </header>
+      <div class="single-tweet">${person["single-tweet"]}</div>
+      <footer>
+        <div class="date-posted">${person["date-posted"]}</div>
+        <div class="action-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `;
 
-  const person = {
-    name: tweetObj.user.name,
-    username: tweetObj.user.handle,
-    "single-tweet": tweetObj.content.text,
-    "date-posted": tweetObj.created_at,
-    "small-avatar": tweetObj.user.avatars
-  }
+    return markup;
+  };
 
-  const markup = `
-  <article>
-  <header>
-    <div class="tweet-head-left">
-      <div class="small-avatar"><img src="${person["small-avatar"]}"></div>
-      <div class="name">${person.name}</div>
-    </div>
-    <div class="username">${person.username}</div>
-  </header>
+  const renderTweets = function(tweetsArray) {
+    for (let i = 0; i < tweetsArray.length; i++) {
+      const newTweet = createTweetElement(tweetsArray[i]);
+      $('#tweets-container').append(newTweet);
+    }
+  };
 
-  <div class="single-tweet">${person["single-tweet"]}</div>
-  <footer>
-    <div class="date-posted">${person["date-posted"]}</div>
-    <div class="action-icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-  </footer>
-  </article>
-  `;
+  renderTweets(data);
 
-
-  // .name = tweetObj.user.name
-  //.username = tweetObj.user.handle
-  //.single-tweet = tweetObj.content.text
-  //.date-posted = tweetObj.created_at
-  //.small-avatar = tweetObj.user.avatars
-  return markup
-
-  }
-
-
-
-// Tweet data:
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
-
-
-})
+});
