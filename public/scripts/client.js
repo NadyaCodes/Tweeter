54bce638480fc60f47cb3001c08effd5c2cb3701
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-27 01:07:36 -0400

    add a function that adds new tweet object to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,54 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function(tweetObj) {
+
+$(document).ready(function() {
+  console.log("client is ready")
+  const createTweetElement = function(tweetObj) {
 
 
-  const $tweet = $(`<article class="tweets">${tweetObj.content.text}</article>`);
+  // const $tweet = $(`<article class="tweets">${tweetObj.content.text}</article>`);
+
+  const person = {
+    name: tweetObj.user.name,
+    username: tweetObj.user.handle,
+    "single-tweet": tweetObj.content.text,
+    "date-posted": tweetObj.created_at,
+    "small-avatar": tweetObj.user.avatars
+  }
+
+  const markup = `
+  <article>
+  <header>
+    <div class="tweet-head-left">
+      <div class="small-avatar"><img src="${person["small-avatar"]}"></div>
+      <div class="name">${person.name}</div>
+    </div>
+    <div class="username">${person.username}</div>
+  </header>
+
+  <div class="single-tweet">${person["single-tweet"]}</div>
+  <footer>
+    <div class="date-posted">${person["date-posted"]}</div>
+    <div class="action-icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+  </article>
+  `;
+
 
   // .name = tweetObj.user.name
   //.username = tweetObj.user.handle
   //.single-tweet = tweetObj.content.text
   //.date-posted = tweetObj.created_at
-  //.avatar = tweetObj.user.avatars
-  return $tweet;
+  //.small-avatar = tweetObj.user.avatars
+  return markup
 
-}
+  }
+
 
 
 // Tweet data:
@@ -37,4 +72,7 @@ const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
 console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); 
+$('#tweets-container').append($tweet);
+
+
+})
